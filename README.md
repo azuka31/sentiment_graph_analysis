cf838b2d0b5762540dc0859b9e8262602d2ad7a9
azuka31 <azkarohbiya@gmail.com>
2025-12-14 17:06:18 +0700

    fixing screenshot

--- README.md
+++ README.md
@@ -4,10 +4,11 @@ This analysis helps understand how to grasp customers feedback without annotatio
 
 # Output
 
-![output1](output/screenshot_2025-12-14_at_16.54.07.png)
-![output2](output/screenshot_2025-12-14_at_16.54.11.png)
-![output3](output/screenshot_2025-12-14_at_16.54.13.png)
-![output4](output/screenshot_2025-12-14_at_16.54.15.png)
-![output5](output/screenshot_2025-12-14_at_16.54.17.png)
-![output6](output/screenshot_2025-12-14_at_16.54.19.png)
-![output7](output/screenshot_2025-12-14_at_16.54.20.png)
+![output1](output/screenshot_2025-12-14_at_17.01.14.png)
+![output2](output/screenshot_2025-12-14_at_17.01.26.png)
+![output3](output/screenshot_2025-12-14_at_17.01.33.png)
+![output4](output/screenshot_2025-12-14_at_17.01.45.png)
+![output5](output/screenshot_2025-12-14_at_17.01.53.png)
+![output6](output/screenshot_2025-12-14_at_17.02.32.png)
+![output7](output/screenshot_2025-12-14_at_17.02.40.png)
+
--- output/edit.sh
+++ output/edit.sh
@@ -1,7 +1,8 @@
-mv Screenshot\ 2025-12-14\ at\ 16.54.07.png screenshot_2025-12-14_at_16.54.07.png
-mv Screenshot\ 2025-12-14\ at\ 16.54.11.png screenshot_2025-12-14_at_16.54.11.png
-mv Screenshot\ 2025-12-14\ at\ 16.54.13.png screenshot_2025-12-14_at_16.54.13.png
-mv Screenshot\ 2025-12-14\ at\ 16.54.15.png screenshot_2025-12-14_at_16.54.15.png
-mv Screenshot\ 2025-12-14\ at\ 16.54.17.png screenshot_2025-12-14_at_16.54.17.png
-mv Screenshot\ 2025-12-14\ at\ 16.54.19.png screenshot_2025-12-14_at_16.54.19.png
-mv Screenshot\ 2025-12-14\ at\ 16.54.20.png screenshot_2025-12-14_at_16.54.20.png
+mv Screenshot\ 2025-12-14\ at\ 17.01.14.png screenshot_2025-12-14_at_17.01.14.png
+mv Screenshot\ 2025-12-14\ at\ 17.01.26.png screenshot_2025-12-14_at_17.01.26.png
+mv Screenshot\ 2025-12-14\ at\ 17.01.33.png screenshot_2025-12-14_at_17.01.33.png
+mv Screenshot\ 2025-12-14\ at\ 17.01.45.png screenshot_2025-12-14_at_17.01.45.png
+mv Screenshot\ 2025-12-14\ at\ 17.01.53.png screenshot_2025-12-14_at_17.01.53.png
+mv Screenshot\ 2025-12-14\ at\ 17.02.04.png screenshot_2025-12-14_at_17.02.04.png
+mv Screenshot\ 2025-12-14\ at\ 17.02.32.png screenshot_2025-12-14_at_17.02.32.png
+mv Screenshot\ 2025-12-14\ at\ 17.02.40.png screenshot_2025-12-14_at_17.02.40.png
