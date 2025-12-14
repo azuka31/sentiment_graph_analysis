6dea109d1a34ce27c89ab9bfb39ba2da375fdb9d
azuka31 <azkarohbiya@gmail.com>
2025-12-14 16:58:47 +0700

    compile all pictures

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ This analysis helps understand how to grasp customers feedback without annotatio
 
 # Output
 
-![Output](output/20251214_result.png)
-
-
+![output1](output/screenshot_2025-12-14_at_16.54.07.png)
+![output2](output/screenshot_2025-12-14_at_16.54.11.png)
+![output3](output/screenshot_2025-12-14_at_16.54.13.png)
+![output4](output/screenshot_2025-12-14_at_16.54.15.png)
+![output5](output/screenshot_2025-12-14_at_16.54.17.png)
+![output6](output/screenshot_2025-12-14_at_16.54.19.png)
+![output7](output/screenshot_2025-12-14_at_16.54.20.png)
---
+++ output/edit.sh
@@ -0,0 +1,7 @@
+mv Screenshot\ 2025-12-14\ at\ 16.54.07.png screenshot_2025-12-14_at_16.54.07.png
+mv Screenshot\ 2025-12-14\ at\ 16.54.11.png screenshot_2025-12-14_at_16.54.11.png
+mv Screenshot\ 2025-12-14\ at\ 16.54.13.png screenshot_2025-12-14_at_16.54.13.png
+mv Screenshot\ 2025-12-14\ at\ 16.54.15.png screenshot_2025-12-14_at_16.54.15.png
+mv Screenshot\ 2025-12-14\ at\ 16.54.17.png screenshot_2025-12-14_at_16.54.17.png
+mv Screenshot\ 2025-12-14\ at\ 16.54.19.png screenshot_2025-12-14_at_16.54.19.png
+mv Screenshot\ 2025-12-14\ at\ 16.54.20.png screenshot_2025-12-14_at_16.54.20.png
